6e428c2bae810008e244f3af76cb41417c28fe5c
ilviocasa <casamassima.silvio@gmail.com>
2016-12-15 19:28:47 -0200

    liquibase

---
+++ liquibase/src/main/java/br/com/selvz/liquibase/App.java
@@ -0,0 +1,13 @@
+package br.com.selvz.liquibase;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Selvz_vr - LiquiBase" );
+    }
+}
